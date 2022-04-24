f4ae4e299f28f0532ad3a975b9e882bbcf1899b7
Mary <regentmarysapro@gmail>
2022-04-24 16:13:10 +0200

    quete poo2

--- Bicycle.php
+++ Bicycle.php
@@ -1,76 +1,15 @@
 <?php
-
-class Bicycle
+require_once 'Vehicle.php';
+class Bicycle extends Vehicle
 {
-    private string $color;
-    private int $nbWheels;
-    private int $nbSeats;
-    private int $currentSpeed; 
+    protected string $color = 'Pink';
+    protected int $nbSeats = 1;
+    protected int $nbWheels = 2;
 
-public function __construct(string $color, int $nbWheels, int $nbSeats, int $currentSpeed)
-	{
-		$this->color = $color;
-        $this->wheels = $nbWheels;
-        $this->seats = $nbSeats;
-        $this->currentSpeed = $currentSpeed;
-	}
-   
-    public function getColor(): string
-    {
-        return $this->color;
-    }
-    public function setColor(string $color): void
+    public function __construct(string $color, int $nbSeats, int $nbWheels)
     {
         $this->color = $color;
-    }  
-
-    public function getCurrentSpeed(): int
-    {
-        return $this->currentSpeed;
-    }
-    public function setCurrentSpeed(int $color): void
-    {
-        $this->currentSpeed;
-    }  
-
-    public function getNbSeats(): int
-    {
-        return $this->nbSeats;
-    }
-    public function setNbSeats(int $nbSeats): void
-    {
         $this->nbSeats = $nbSeats;
-    }  
-
-    public function getNbWheels(): int
-    {
-        return $this->nbWheels;
-    }
-    public function setNbWheels(int $nbWheels): void
-    {
-        $this->nbWheels = $nbWheels;
-    }
-
-    public function forward(): string
-    {
-    $this->currentSpeed = 15;
-
-    return "Go !";
-    }
-
-    public function brake(): string
-    {
-   $sentence = "";
-   while ($this->currentSpeed > 0) {
-       $this->currentSpeed--;
-       $sentence .= "Brake !!!";
-   }
-   $sentence .= "I'm stopped !";
-   return $sentence;
-    }
-
-    public function dump()
-    {
-    var_dump($this);
+        $this->wheels = $nbWheels;
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,12 +2,21 @@
 
 require_once '../Car.php';
 require_once '../Bicycle.php';
+require_once '../Vehicle.php';
+require_once '../Truck.php';
 
-$porsche = new Car("blue", "gazol", 5);
-$bmx = new bicycle("pink", 2, 1, 20);
-var_dump($porsche, $bmx);
-echo $porsche->dump($porsche, $bmx);
+$vehicle = new Vehicle('Blue', 3, 0, 0);
+$car = new Car('green', 4, 'electric');
+$bicycle = new Bicycle('Pink', 1, 2);
+$truck = new Truck ('Yellow', 3, 'Fuel', 0);
 
-echo $porsche->start();
-echo $porsche->forward();
-echo $porsche->brake();
+//echo $bicycle->forward();
+//echo $bicycle->brake();
+//echo $car->forward();
+//echo $car->brake();
+echo $truck->forward();
+echo $truck->brake();
+echo $truck->setStorageCapacity(0, 1);
+
+var_dump($vehicle, $car, $bicycle, $truck);
+var_dump(Car::ALLOWED_ENERGIES);
