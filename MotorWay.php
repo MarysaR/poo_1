4648dc9ea0cb754576125f663e80f0c2f8777da1
Mary <regentmarysapro@gmail>
2022-05-02 12:31:02 +0200

    quete poo 4

---
+++ MotorWay.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once 'HighWay.php';
+
+final class MotorWay extends HighWay
+{
+    public function addVehicle(Vehicle $vehicle): array
+    {
+       $bike = new Bicycle('Pink', 1, 2);
+       if ($vehicle instanceof $bike){
+           echo "interdit sur l'autoroute !!". PHP_EOL;        
+        }
+        $this->setCurrentVehicles($vehicle);
+        return $this->currentVehicles;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,16 +4,21 @@ require_once '../Car.php';
 require_once '../Bicycle.php';
 require_once '../Vehicle.php';
 require_once '../Truck.php';
+require_once '../HighWay.php';
+
 
 $vehicle = new Vehicle('Blue', 3, 0, 0);
 $car = new Car('green', 4, 'electric');
 $bicycle = new Bicycle('Pink', 1, 2);
 $truck = new Truck ('Yellow', 3, 'Fuel', 0);
+$resident = new ResidentWay(2, 50);
+$motor = new MotorWay (4, 130);
+$pedestrian = new PedestrianWay(1, 10);
 
-//echo $bicycle->forward();
-//echo $bicycle->brake();
-//echo $car->forward();
-//echo $car->brake();
+$motor->addVehicle($bike);
+$pedestrian->addVehicle($car);
+
+
 echo $truck->forward();
 echo $truck->brake();
 echo $truck->setStorageCapacity(0, 1);
