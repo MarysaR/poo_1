a1a451bd1020624ed0428615fd4874a50766c56b
Mary <regentmarysapro@gmail>
2022-05-02 12:35:17 +0200

    Revert "quete poo 4"
    This reverts commit 4648dc9ea0cb754576125f663e80f0c2f8777da1.

--- public/index.php
+++ public/index.php
@@ -4,21 +4,16 @@ require_once '../Car.php';
 require_once '../Bicycle.php';
 require_once '../Vehicle.php';
 require_once '../Truck.php';
-require_once '../HighWay.php';
-
 
 $vehicle = new Vehicle('Blue', 3, 0, 0);
 $car = new Car('green', 4, 'electric');
 $bicycle = new Bicycle('Pink', 1, 2);
 $truck = new Truck ('Yellow', 3, 'Fuel', 0);
-$resident = new ResidentWay(2, 50);
-$motor = new MotorWay (4, 130);
-$pedestrian = new PedestrianWay(1, 10);
 
-$motor->addVehicle($bike);
-$pedestrian->addVehicle($car);
-
-
+//echo $bicycle->forward();
+//echo $bicycle->brake();
+//echo $car->forward();
+//echo $car->brake();
 echo $truck->forward();
 echo $truck->brake();
 echo $truck->setStorageCapacity(0, 1);
